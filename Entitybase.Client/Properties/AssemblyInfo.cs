fa2b87ceb14d9c7cb43f9790aaeae37acd0e42a4
shantiw <shantiw3@outlook.com>
2018-05-25 13:28:30 +0800

    Entitybase

--- Entitybase.Client/Properties/AssemblyInfo.cs
+++ Entitybase.Client/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Entitybank.Client")]
+[assembly: AssemblyTitle("Entitybase.Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Entitybank.Client")]
+[assembly: AssemblyProduct("Entitybase.Client")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Entitybase.WpfApp/Properties/AssemblyInfo.cs
+++ Entitybase.WpfApp/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Entitybank.WpfApp")]
+[assembly: AssemblyTitle("Entitybase.WpfApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Entitybank.WpfApp")]
+[assembly: AssemblyProduct("Entitybase.WpfApp")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
